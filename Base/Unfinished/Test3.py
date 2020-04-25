ead424c7142bd423a082dbc4d9981f888a6ba82c
1017969513@qq.com <jiangjiali77>
2020-04-25 15:21:50 +0800

    complete

--- Base/Unfinished/Test3.py
+++ Base/Unfinished/Test3.py
@@ -2,4 +2,25 @@
 '''
 题目：一个整数，它加上100后是一个完全平方数，再加上168又是一个完全平方数，请问该数是多少？
 
+
+a + 100 = n*n
+a + 100 + 168 = m*m
+
+m*m - n*n = 168
+(m+n)*(m-n) = 168
+
+m + n   肯定是偶数
+
+m - n    肯定也是偶数，那么m是
+
 '''
+def squareNumber(b,c):
+    for i in range(1,int(b/2+1)):
+        if i%2 == 0:
+            j = b/i
+            if j%2 == 0 and i>j:
+                m = (i+j)/2
+                n = (i-j)/2
+                a = n*n - c
+                print(a)
+squareNumber(168,100)
