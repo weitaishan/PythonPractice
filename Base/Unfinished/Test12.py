068964b3872c09906bc204adc770bba68ccefb28
魏太山 <450785730@qq.com>
2020-05-02 22:33:57 +0800

    complete Test12

--- Base/Unfinished/Test12.py
+++ Base/Unfinished/Test12.py
@@ -5,3 +5,33 @@
 
 
 '''
+list = []
+zs = 1
+for i in range(101, 201):
+    for j in range(2, i):
+        if i%j == 0:
+            zs = 0
+    if zs == 1:
+        list.append(i)
+    zs = 1
+print(list)
+
+
+
+
+
+
+
+'''
+list1 = []
+list2 = []
+for i in range(101, 201):
+    for j in range(2, i):
+        a = i % j
+        list1.append(a)
+    if 0 not in list1:
+        list2.append(i)
+    list1 = []
+
+print(list2)
+'''
