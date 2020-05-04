7b50fd7bd40a1213a3454e440a364ac0bc0f556c
1017969513@qq.com <jiangjiali77>
2020-05-04 13:44:57 +0800

    输出101-200之间的所有素数。

--- Base/Unfinished/Test12.py
+++ Base/Unfinished/Test12.py
@@ -5,6 +5,33 @@
 
 
 '''
+
+#方法三
+'''
+思考：写出一定数量的素数，可发现个位数的素数有：1、2、3、5、7，但是多位数的素数都是以1、3、7、9结尾。
+所以可以先排除偶数和5的倍数的非个位数。进行取模运算的时候，不需要将所有小于的数都除一遍。
+'''
+import math
+list = []
+zs = 1
+for i in range(101, 201):
+    if i % 2 == 0 or i % 5 == 0:
+        continue
+    a = math.sqrt(i)
+    b = int(a) + 1
+    for j in range(2, b):
+        if i % j == 0:
+            zs = 0
+    if zs == 1:
+        list.append(i)
+    zs = 1
+print(list)
+
+
+
+'''
+方法二：
+
 list = []
 zs = 1
 for i in range(101, 201):
@@ -15,14 +42,12 @@ for i in range(101, 201):
         list.append(i)
     zs = 1
 print(list)
-
-
-
-
-
+'''
 
 
 '''
+方法一：
+
 list1 = []
 list2 = []
 for i in range(101, 201):
