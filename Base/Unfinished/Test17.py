a7756981b8becd565301638b892b49f067518151
1017969513@qq.com <jiangjiali77>
2020-05-04 22:26:40 +0800

    Test17:输入一行字符，分别统计出其中英文字母、空格、数字和其他字符的个数。

--- Base/Unfinished/Test17.py
+++ Base/Unfinished/Test17.py
@@ -5,3 +5,19 @@
 
 程序分析：利用 while 或 for 语句,条件为输入的字符不为 '\n'。
 '''
+list = list(input("请输入一串字符： "))
+print(list)
+list1 = [chr(i) for i in range(97, 123)]
+list2 = [chr(x) for x in range(65, 91)]
+list3 = ['0','1','2','3','4','5','6','7','8','9']
+a,b,c,d = 0,0,0,0
+for i in list:
+    if i == " ":
+        a += 1
+    elif i in list3:
+        b += 1
+    elif i in list1 or i in list2:
+        c += 1
+    else:
+        d = len(list) - a - b - c
+print(a, b, c, d)
