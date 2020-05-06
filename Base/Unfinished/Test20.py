c0770e9d2b9af6e0363e83ecfa15be42166e1fd1
1017969513@qq.com <jiangjiali77>
2020-05-06 12:39:25 +0800

    Test20：小球自由落下，计算n次后的高度及n次后经过的总米数。

--- Base/Unfinished/Test20.py
+++ Base/Unfinished/Test20.py
@@ -5,3 +5,22 @@
 
 
 '''
+
+
+'''
+分析：可以使用递归方法
+f(1)=100    f(2)=50     f(n) = f(n-1)/2 n>1
+'''
+
+# 求出反弹多高
+def ball(n):
+    if n <= 1:
+        return 100
+    return ball(n-1)/2
+print(ball(5))
+
+a = int(input("请输入球落地的次数： "))
+sum = 0
+for i in range(1, a+1):
+    sum += 2*ball(i)
+print(sum-ball(1))
