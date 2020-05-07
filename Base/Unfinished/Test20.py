60840afbff3aeaa97980aa374f362eb96dc5c8f2
1017969513@qq.com <jiangjiali77>
2020-05-07 23:01:48 +0800

    Test20：求一个球自由下落，落地n次的高度和经过的总米数

--- Base/Unfinished/Test20.py
+++ Base/Unfinished/Test20.py
@@ -13,14 +13,26 @@ f(1)=100    f(2)=50     f(n) = f(n-1)/2 n>1
 '''
 
 # 求出反弹多高
+'''
 def ball(n):
     if n <= 1:
         return 100
     return ball(n-1)/2
 print(ball(5))
+'''
 
 # a = int(input("请输入球落地的次数： "))
 # sum = 0
 # for i in range(1, a+1):
 #     sum += 2*ball(i)
 # print(sum-ball(1))
+
+
+# 使用循环解题（求高度）
+number = int(input("请输入小球的落地次数： "))
+a = 100
+b = 0
+for i in range(1, number):
+    a = a/2
+    b += 2*a
+print(a, b+100)
