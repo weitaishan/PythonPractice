d5055b7717786ea0b118a9239ac5e890d350823e
1017969513@qq.com <jiangjiali77>
2020-05-08 21:55:18 +0800

    Test32：反向输出列表值

--- Base/Unfinished/Test32.py
+++ Base/Unfinished/Test32.py
@@ -5,3 +5,18 @@
 
 
 '''
+
+
+
+list = [32, 'name', '土豆',999, 110]
+
+
+#方法一
+'''
+for i in range(1, len(list)+1):
+    print(list[len(list)-i], end =" ")
+'''
+
+#方法二
+list.reverse()     # 注意：reverse()方法是修改列表，而不是返回一个新的列表
+print(list)
