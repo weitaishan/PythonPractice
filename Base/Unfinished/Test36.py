e1ea233d1523afb46a665c96618e3a65ef0067d2
1017969513@qq.com <jiangjiali77>
2020-05-10 17:10:46 +0800

    Test36：求100以内的素数

--- Base/Unfinished/Test36.py
+++ Base/Unfinished/Test36.py
@@ -3,5 +3,21 @@
 '''
 题目：求100之内的素数。
 
+质数又称素数。一个大于1的自然数，除了1和它自身外，不能被其他自然数整除的数叫做质数；否则称为合数。
 
 '''
+import math
+list = []
+zs = 1
+for i in range(2, 101):
+    if i % 2 == 0:
+        continue
+    a = math.sqrt(i)
+    b = int(a) + 1
+    for j in range(2, b):
+        if i % j == 0:
+            zs = 0
+    if zs == 1:
+        list.append(i)
+    zs = 1
+print(list)
