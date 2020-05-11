a70961b08a8ca8fcbdd138641322eef2058754c4
1017969513@qq.com <jiangjiali77>
2020-05-11 22:19:44 +0800

    Test23：打印出菱形图形

--- Base/Unfinished/Test23.py
+++ Base/Unfinished/Test23.py
@@ -12,7 +12,33 @@
    *
 '''
 
-
-for i in range(1,8):
+'''
+a = 4
+b = 0
+c = 0
+for i in range(1, 8):
     if i % 2 != 0:
-        print("*" * i)
+        a -= 1
+        print(' ' * a + "*" * i)
+for j in range(1, 6):
+    if j % 2 != 0:
+        b = 6 - j
+        c += 1
+        print(' ' * c + "*" * b)
+'''
+
+
+def star(n):
+    a = (n-1)//2 + 1
+    b = 0
+    c = 0
+    for i in range(1, n+1):
+        if i % 2 != 0:
+            a -= 1
+            print(' ' * a + "*" * i)
+    for j in range(1, n-1):
+        if j % 2 != 0:
+            b = n-1 - j
+            c += 1
+            print(' ' * c + "*" * b)
+star(11)
