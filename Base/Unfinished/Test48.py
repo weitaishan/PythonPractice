ddba05e96eb08272e4b59fbdb438a6d5494c3b8b
1017969513@qq.com <jiangjiali77>
2020-05-12 22:28:02 +0800

    Test48：比较两个数的大小

--- Base/Unfinished/Test48.py
+++ Base/Unfinished/Test48.py
@@ -5,3 +5,14 @@
 
 比较2个数字的大小
 '''
+
+
+def compare(x, y):
+    if x - y > 0:
+        print("%d大于%d" % (x, y))
+    elif x - y == 0:
+        print("%d等于%d" % (x, y))
+    else:
+        print("%d小于%d" % (x, y))
+
+compare(1, 3)
