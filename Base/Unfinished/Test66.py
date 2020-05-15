40bcaadde70131acf68f9340d612e1731ce91869
1017969513@qq.com <jiangjiali77>
2020-05-15 22:30:43 +0800

    Test66：输入三个数，按大小顺序输出

--- Base/Unfinished/Test66.py
+++ Base/Unfinished/Test66.py
@@ -5,3 +5,21 @@
 
 
 '''
+
+def compare(a, b, c):
+    if a > b:
+        x = a
+        a = b
+        b = x
+    if a > c:
+        y = a
+        a = c
+        c = y
+    if b > c:
+        z = b
+        b = c
+        c = z
+    print(c, b, a)    #从大到小排序
+    print(a, b, c)    #从小到大排序
+
+compare(34, 5, 78)
