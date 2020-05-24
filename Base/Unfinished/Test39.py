0317d17ada6687983e3ceb06b53d3c6d5d5dbb4c
1017969513@qq.com <jiangjiali77>
2020-05-24 15:58:57 +0800

    Test39：往一个排好序的数组插入一个数，并按原来的排序规律打印出来

--- Base/Unfinished/Test39.py
+++ Base/Unfinished/Test39.py
@@ -21,42 +21,42 @@
 # print(sortList)
 
 # 方法二
-sortList = [0, 6, 7, 13, 14, 70, 78, 81, 89]
-emptyList = []
-number = int(input("请输入一个数： "))
-if number > sortList[-1]:
-    sortList.append(number)
-else:
-    for i in range(0, len(sortList)):
-        if number < sortList[i]:
-            emptyList.append(number)
-            break
-        else:
-            emptyList.append(sortList[i])
-    for j in range(i, len(sortList)):
-        emptyList.append(sortList[j])
-print(emptyList)
-
-
-# 方法二的改进
-sortList = [0, 6, 7, 13, 14, 70, 78, 81, 89]
-emptyList = []
-number = int(input("请输入一个数： "))
-if number > sortList[-1]:
-    sortList.append(number)
-else:
-    isAppend = 0
-    for i in range(0, len(sortList)):
-        if number < sortList[i]:
-            if isAppend == 0:
-                emptyList.append(number)
-                emptyList.append(sortList[i])
-                isAppend = 1
-            else:
-                emptyList.append(sortList[i])
-        else:
-            emptyList.append(sortList[i])
-print(emptyList)
+# sortList = [0, 6, 7, 13, 14, 70, 78, 81, 89]
+# emptyList = []
+# number = int(input("请输入一个数： "))
+# if number > sortList[-1]:
+#     sortList.append(number)
+# else:
+#     for i in range(0, len(sortList)):
+#         if number < sortList[i]:
+#             emptyList.append(number)
+#             break
+#         else:
+#             emptyList.append(sortList[i])
+#     for j in range(i, len(sortList)):
+#         emptyList.append(sortList[j])
+# print(emptyList)
+#
+#
+# # 方法二的改进
+# sortList = [0, 6, 7, 13, 14, 70, 78, 81, 89]
+# emptyList = []
+# number = int(input("请输入一个数： "))
+# if number > sortList[-1]:
+#     sortList.append(number)
+# else:
+#     isAppend = 0
+#     for i in range(0, len(sortList)):
+#         if number < sortList[i]:
+#             if isAppend == 0:
+#                 emptyList.append(number)
+#                 emptyList.append(sortList[i])
+#                 isAppend = 1
+#             else:
+#                 emptyList.append(sortList[i])
+#         else:
+#             emptyList.append(sortList[i])
+# print(emptyList)
 
 
 
@@ -74,3 +74,19 @@ print(emptyList)
 # print(sortList)
 
 
+
+# 方法三的改进
+sortList = [0, 6, 7, 13, 14, 70, 78, 81, 89]
+number = int(input("请输入一个数： "))
+sortList.append(number)
+a = -1
+judge = 1
+while judge:
+    if sortList[a] < sortList[a-1]:
+        t = sortList[a-1]
+        sortList[a-1] = sortList[a]
+        sortList[a] = t
+        a -= 1
+    else:
+        judge = 0
+print(sortList)
