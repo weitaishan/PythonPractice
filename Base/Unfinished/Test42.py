e38affa8245518fc2813964fa8850d23408bcfce
1017969513@qq.com <jiangjiali77>
2020-05-30 15:27:51 +0800

    Test42：学习使用auto定义变量的用法

--- Base/Unfinished/Test42.py
+++ Base/Unfinished/Test42.py
@@ -5,3 +5,50 @@
 
 程序分析：没有auto关键字，使用变量作用域来举例吧。
 '''
+
+num = 2
+def autoFunc():
+    num = 1
+    print("internal = %d" % num)
+    num += 1
+    print(num)
+for i in range(10):
+    print("The number = %d" % num)
+    num += 1
+    autoFunc()
+
+
+'''
+程序运行结果：
+The number = 2
+internal = 1
+2
+The number = 3
+internal = 1
+2
+The number = 4
+internal = 1
+2
+The number = 5
+internal = 1
+2
+The number = 6
+internal = 1
+2
+The number = 7
+internal = 1
+2
+The number = 8
+internal = 1
+2
+The number = 9
+internal = 1
+2
+The number = 10
+internal = 1
+2
+The number = 11
+internal = 1
+2
+
+'''
