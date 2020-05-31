bdaacc2f3134d4e1489f1e825b90833c5bb497c4
1017969513@qq.com <jiangjiali77>
2020-05-31 13:11:55 +0800

    Test57:学习使用line画直线（熟悉python中的tkinter模块）

--- Base/Unfinished/Test57.py
+++ Base/Unfinished/Test57.py
@@ -6,3 +6,30 @@
 
 
 '''
+'''
+python中tkinter模块的学习：http://request.uml.com.cn/chengzhang/mobile/oneSubject.asp?wID=902&utype=kf
+
+'''
+# from tkinter import *
+# tk = Tk()
+# canvas = Canvas(tk, width=500, heigth=500)
+# canvas.pack()
+# canvas.create_line(0, 0, 500, 500)
+
+
+
+from tkinter import *
+class line:
+    def __init__(self):
+        canvas = Canvas(width=500, height=500, bg="yellow")
+        canvas.pack()
+        x0, y0, x1, y1 = 10, 10, 500, 500
+        for i in range(25):
+            canvas.create_line(x0, y0, x1, y1, width=1, fill="red")
+            x0 += 10
+            y0 += 10
+            x1 -= 10
+            y1 -= 10
+        mainloop()
+if __name__ == "__main__":
+    a_line = line()
