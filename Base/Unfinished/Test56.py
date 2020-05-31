a9d65f19941cc787e7bfd281080c107cbef33769
1017969513@qq.com <jiangjiali77>
2020-05-31 15:38:12 +0800

    Test56:画图，学习使用circle画圆形

--- Base/Unfinished/Test56.py
+++ Base/Unfinished/Test56.py
@@ -3,6 +3,12 @@
 '''
 题目：画图，学用circle画圆形。　　　
 
-
+学习网址：http://wenku.uml.com.cn/document/mobile/2020011043.html
 
 '''
+from tkinter import *
+root = Tk()
+cv = Canvas(root, bg='green')
+cv.pack()
+cv.create_oval((10, 10, 210, 210), fill='yellow')
+root.mainloop()
