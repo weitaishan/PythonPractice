a253a722837fa4ad36f8074fdd6f609a951a11e1
1017969513@qq.com <jiangjiali77>
2020-06-03 21:45:33 +0800

    Test63:使用tkinter模块画椭圆

--- Base/Unfinished/Test63.py
+++ Base/Unfinished/Test63.py
@@ -6,3 +6,10 @@
 程序分析：使用 Tkinter。
 
 '''
+
+from tkinter import *
+root = Tk()    # 创建一个Tk对象
+cv = Canvas(root, bg = 'white')   # 创建一个Canvas，背景设置为白色
+cv.create_oval((10, 10, 210, 110), fill = 'red')   # 创建一个长200，宽100的椭圆，并用红色填充
+cv.pack()
+root.mainloop()
