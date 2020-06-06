f016737eb47bfe502318514a298470058fd68219
1017969513@qq.com <jiangjiali77>
2020-06-06 22:50:46 +0800

    Test64:利用ellipse和rectangle画图

--- Base/Unfinished/Test64.py
+++ Base/Unfinished/Test64.py
@@ -7,4 +7,15 @@
 
 '''
 
-
+from tkinter import *
+root = Tk()
+cv = Canvas(root, bg = "yellow")
+left = 10
+top = 10
+right = 20
+buttom = 20
+for i in range(0, 10):
+    cv.create_rectangle(left + 2*i, top + 2*i, right + 2*i, buttom + 2*i, fill = 'green', width = 5)
+    cv.create_oval(left + 4*i, top + 4*i, right + 4*i, buttom + 4*i, fill = 'blue')
+cv.pack()
+root.mainloop()
