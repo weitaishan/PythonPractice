aa64aa5f4a56d806cde4f66d2ec386c271265da0
1017969513@qq.com <jiangjiali77>
2020-06-07 16:27:09 +0800

    Test67:将数组中最大的数与第一个元素交换，最小的数与最后一个元素交换

--- Base/Unfinished/Test67.py
+++ Base/Unfinished/Test67.py
@@ -6,3 +6,24 @@
 
 
 '''
+
+list = [12, 6, 90, 57, 7, 99, 239, 81]
+a = 0
+b = 0
+# 找最小数的索引
+for i in range(0, len(list)-1):
+    if list[a] > list[i+1]:
+        a = i+1
+# 找最大数的索引
+for j in range(0, len(list)-1):
+    if list[b] < list[j+1]:
+        b = j+1
+# 将最小的数与最后一个元素交换
+x = list[a]
+list[a] = list[-1]
+list[-1] = x
+# 将最大的数与第一个元素交换
+y = list[b]
+list[b] = list[0]
+list[0] = y
+print(list)
