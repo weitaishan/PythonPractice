e404240f5a5daa633aa1b86d0e0b22a5ca0d0916
1017969513@qq.com <jiangjiali77>
2020-06-21 15:53:29 +0800

    Test78:找到年龄最大的人，并输出

--- Base/Unfinished/Test78.py
+++ Base/Unfinished/Test78.py
@@ -8,3 +8,16 @@
 
 
 '''
+
+
+ageList = [35, 26, 9, 66, 71, 99, 86, 29]
+# 方法一
+print(max(ageList))
+
+# 方法二
+for i in range(0, len(ageList)-1):
+    if ageList[i] > ageList[i+1]:
+        a = ageList[i]
+        ageList[i] = ageList[i+1]
+        ageList[i+1] = a
+print(ageList[-1])
