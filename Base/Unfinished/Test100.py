e33e375a3b5b7b787529f6cae26d6ee7fcebbf88
1017969513@qq.com <jiangjiali77>
2020-06-21 19:51:56 +0800

    Test100：列表转换为字典

--- Base/Unfinished/Test100.py
+++ Base/Unfinished/Test100.py
@@ -5,3 +5,29 @@
 
 
 '''
+
+# 方法一：嵌套列表转字典
+list1 = [["sex", "boy"], ["name", "Jenny"], ["age", 18]]
+print(dict(list1))
+
+
+
+# 方法二：嵌套列表转字典
+list1 = [["sex", "boy"], ["name", "Jenny"], ["age", 18]]
+dic = {}
+for i in list1:
+    dic[i[0]] = i[1]   # 字典赋值，左边为key，右边为value
+print(dic)
+
+# 方法三：嵌套列表转字典
+list4 = ['tyc=ddo56365f', 'cmb=13652d22d', 'eay=53896d' ]
+newDict = {}
+for i in range(len(list4)):
+    newDict[list4[i]] = list4[i].split('=')[1]
+print(newDict)
+
+# 方法四：两个列表转换为字典。使用zip函数   zip函数学习地址：https://www.runoob.com/python3/python3-func-zip.html
+list2 = ["sex", "name", "age"]
+list3 = ["boy", "Jenny", 18]
+print(dict(zip(list2, list3)))
+
