5ba8e235a64f8fd4a1049bafe22cc2f5c9ef2a6c
Administrator <1017969513@qq.com>
2020-06-23 16:27:57 +0800

    Test86：两个字符串连接程序

--- Base/Unfinished/Test86.py
+++ Base/Unfinished/Test86.py
@@ -5,3 +5,15 @@
 
 
 '''
+
+# 方法一
+def connect(str1, str2):
+    print(str1+str2)
+connect("123", "你好")
+
+# 方法二
+def link():
+    str1 = input("请输入第一个字符串： ")
+    str2 = input("请输入第二个字符串： ")
+    print(str1+str2)
+link()
