6785fc6d75ba5fdfd14ba6dc8fdb0764626b2bc2
魏太山 <450785730@qq.com>
2020-07-15 15:31:57 +0800

    简单爬虫基础架构

--- small_project/baike_spider/html_outputer.py
+++ small_project/baike_spider/html_outputer.py
@@ -1,1 +1,15 @@
 # *_*coding:utf-8 *_*
+
+#本类是一个蒋data输出成一个html的方法
+class HtmlOutputer(object):
+
+    def __init__(self):
+        self.datas = []
+
+    #定义一个收集数据的方法，将爬取的data存入datas当中
+    def collect_data(self, data):
+        pass
+
+    #定义一个输出到html的方法，将datas里面的数据用html的格式输出
+    def output_html(self):
+        pass
--- small_project/baike_spider/html_parser.py
+++ small_project/baike_spider/html_parser.py
@@ -1,1 +1,20 @@
 # *_*coding:utf-8 *_*
+
+import re
+import urllib.parse
+from  bs4 import BeautifulSoup
+
+#本类是一个html内容的解析器，用于解析html中的内容
+class HtmlParser(object):
+
+    #公开方法，用BeautifulSoup去解析下载的url中的内容
+    def parse(self, page_url, html_cont):
+        pass
+
+    #私有方法，获取新的url
+    def _get_new_urls(self, page_url, soup):
+        pass
+
+    #私有方法，获取新的data
+    def _get_new_data(self, page_url, soup):
+        pass
